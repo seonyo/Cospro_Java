31ff5c28fc17ca960a83672b63a638e2072e793a
seonyo <jsy6535@naver.com>
2023-04-05 17:15:46 +0900

    문제를 잘못 이해하여 수정함 - 문제 13(배열을 String에 이어붙이기)

--- Cospro/src/D0405/ArrString.java
+++ Cospro/src/D0405/ArrString.java
@@ -3,7 +3,9 @@ package D0405;
 class Solution{
 	public String solution (String[] words) {
 		String answer = "";
-		for(int i=0; i<words.length; i++) answer+=words[i];
+		for(int i=0; i<words.length; i++) 
+			if(words[i].length()>=5) answer+=words[i];
+		if(answer=="") answer="empty";
 		return answer;
 	}
 }
