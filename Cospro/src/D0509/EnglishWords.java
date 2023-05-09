74719e775393c515ecdca2cce70d56c5c3f7a16f
seonyo <jsy6535@naver.com>
2023-05-09 16:57:39 +0900

    FEAT: 문제 21(영단어 오타 갯수)

---
+++ Cospro/src/D0509/EnglishWords.java
@@ -0,0 +1,28 @@
+package D0509;
+
+class EnglishWords {
+ public int solution(String[] words, String word) {
+     // 여기에 코드를 작성해주세요.
+     int count = 0;
+     for(int i=0; i<words.length; i++) {
+    	 for(int j=0; j<words[i].length(); j++) {
+    		 if(!(words[i].charAt(j) == word.charAt(j))) {
+    			 count++;
+    		 }
+    	 }
+     }
+     
+     return count;
+ }
+ 
+ // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+ public static void main(String[] args) {
+     EnglishWords sol = new EnglishWords();
+     String[] words = {new String("CODE"), new String("COED"), new String("CDEO")};
+     String word = new String("CODE");
+     int ret = sol.solution(words, word);
+     
+     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+     System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+ }
+}
